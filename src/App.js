e837d8c9a75b6424728ac8d84b028246c8189c88
Yiyi <loveloveyy@hotmail.com>
2022-02-07 14:04:30 +0100

    avec routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+
+  const [data,setData]=useState();
+  const [isLoading, setIsLoading]=useState(true)
+
+  useEffect(()=>{
+    const fetchData = async()=>{
+      try{
+        const response = await axios.get("https://my-deliveroo-backend-project.herokuapp.com/")
+        console.log(response.data);
+      }
+    }
+  },[])
   return (
     <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+      <h1></h1>
+      <p></p>
+      <img src="" alt="" />
     </div>
   );
 }
