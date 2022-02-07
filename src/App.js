82873d73306188398adceec73b52473100554f16
Yiyi <loveloveyy@hotmail.com>
2022-02-07 17:24:51 +0100

    avec condition d'affichage de categories

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
-  const [data,setData]=useState();
-  const [isLoading, setIsLoading]=useState(true)
-
-  useEffect(()=>{
-    const fetchData = async()=>{
-      try{
-        const response = await axios.get("https://my-deliveroo-backend-project.herokuapp.com/")
+  useEffect(() => {
+    const fetchData = async () => {
+      // console.log("useEffect est déclenchée...");
+      try {
+        const response = await axios.get(
+          "https://my-deliveroo-backend-project.herokuapp.com/"
+        );
         console.log(response.data);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.response);
       }
-    }
-  },[])
-  return (
-    <div>
-      <h1></h1>
-      <p></p>
-      <img src="" alt="" />
+    };
+    fetchData();
+  }, []);
+  return isLoading ? (
+    <span>En cours de chargement...</span>
+  ) : (
+    <div className="app">
+      <header>
+        <div className="text">
+          <h1>{data.restaurant.name}</h1>
+          <p>{data.restaurant.description}</p>
+        </div>
+        <img src={data.restaurant.picture} alt="" />
+      </header>
+
+      <div className="container">
+        {data.categories.map((category, index) => {
+          return (
+            data.categories.meals.length > 0 && (
+              <div>
+                <h2 key={index}>{category.name}</h2>
+                <div className="categoryBlock">
+                  {category.meals.map((meal, index) => {
+                    console.log(meal);
+                    return (
+                      <div className="meal" key={index}>
+                        <div>
+                          <h3>{meal.title}</h3>
+                          <p>{meal.description}</p>
+                          <p>
+                            <span>{meal.price} €</span>
+                            {
+                              (meal.popular = true && (
+                                <span className="populaire"> Populaire</span>
+                              ))
+                            }
+                          </p>
+                        </div>
+                        <div>
+                          {meal.picture && <img src={meal.picture} alt="" />}
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )
+          );
+        })}
+        <div className="panierBlock">
+          <p>Valider mon panier</p>
+          <p>Votre panier est vide</p>
+        </div>
+      </div>
     </div>
   );
 }
